960b313aef8c1237accd71474cfede48b1a6ec04
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-04-22 21:24:22 +0100

    fixed import errors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .alchemy_database import engine
-from routers import user, post, auth, vote
+from .routers import user, post, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
